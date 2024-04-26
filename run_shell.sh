ae6848c8a13f9eae0a27e188e40fdb3ccdb56439
Zafaryab Haider (EECE - Devabhaktuni) <zhaider@login1.localdomain>
2024-04-26 12:48:13 -0400

    INIT

---
+++ run_shell.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=comp               
+#SBATCH --partition=gpu               
+#SBATCH --mail-user=zafaryab.haider@maine.edu  
+#SBATCH --mem=100gb   
+#SBATCH --output=log/run_check_SC15b_%j.log         
+# SBATCH --gres=gpu:a100:2
+#SBATCH --gres=gpu:l40
+
+module load apptainer
+echo "Running Script"
+# export PYTORCH_CUDA_ALLOC_CONF="max_split_size_mb:128"
+apptainer run --nv competition.simg python -m src.eval_f1 --true_labels_file train_data.parquet --pred_file predictions.parquet
+# apptainer run --nv competition.simg python -m src.ml_gen_detection.dagpap24_baseline
+# apptainer run --nv competition.simg python parquet_analyse.py
